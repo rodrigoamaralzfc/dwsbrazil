ecff463ddedeef83baf0e28d5b8b1b7a7d7558d8
Rodrigo A. do Amaral <rodrigo.zfc@gmail.com>
2025-11-15 23:44:55 -0300

    feat: create routing and React Query client

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,25 @@
+import { HomePage } from "@/pages/HomePage";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import { Header } from "./components/Header/Header";
+import { NotFoundPage } from "./pages/404";
+import { PostDetailPage } from "./pages/PostDetailPage";
+
+const WithHeaderLayout = () => (
+  <>
+    <Header />
+    <Outlet />
+  </>
+)
+
+export const AppRoutes = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route element={<WithHeaderLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/post/:id" element={<PostDetailPage />} />
+      </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
+);
